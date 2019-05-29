85ce2b903c662a59594c926421ec6a9a522711e8
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 04:12:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,8 @@ def assign_rooms(names)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+batch_badge_creator(attendees).each do |item|
+    puts item
+  end
   # puts assign_rooms(attendees)
 end
